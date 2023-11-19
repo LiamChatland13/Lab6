6c25132a59ac66834a10cbdb4501340ca83834ba
liamc <liamc@LIAMSXPS15>
2023-11-19 16:58:29 -0500

    Add project files.

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Lab6.Data;
+using Lab6.Models;
+
+
+namespace Lab6.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly StudentDbContext _context;
+
+        public StudentsController(StudentDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Students
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)] // Successful response when returning list of students
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)] // Internal server error response
+        public async Task<ActionResult<IEnumerable<Student>>> GetStudents()
+        {
+            try
+            {
+                return Ok(await _context.Students.ToListAsync());
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+
+        // GET: api/Students/5
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)] // Successful response when returning a student by ID
+        [ProducesResponseType(StatusCodes.Status404NotFound)] // Not found response if student doesn't exist
+        public async Task<ActionResult<Student>> GetStudent(Guid id)
+        {
+            var student = await _context.Students.FindAsync(id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(student);
+        }
+
+        // PUT: api/Students/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)] // Successful update response
+        [ProducesResponseType(StatusCodes.Status400BadRequest)] // Bad request if the IDs don't match
+        [ProducesResponseType(StatusCodes.Status404NotFound)] // Not found if the student doesn't exist
+        public async Task<IActionResult> PutStudent(Guid id, Student student)
+        {
+            if (id != student.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(student).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StudentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Students
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)] // Successful creation response
+        [ProducesResponseType(StatusCodes.Status400BadRequest)] // Bad request if there are validation errors or malformed data
+        public async Task<ActionResult<Student>> PostStudent(Student student)
+        {
+            _context.Students.Add(student);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetStudent", new { id = student.Id }, student);
+        }
+
+        // DELETE: api/Students/5
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)] // Successful deletion response
+        [ProducesResponseType(StatusCodes.Status404NotFound)] // Not found if the student doesn't exist
+        public async Task<IActionResult> DeleteStudent(Guid id)
+        {
+            var student = await _context.Students.FindAsync(id);
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            _context.Students.Remove(student);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool StudentExists(Guid id)
+        {
+            return _context.Students.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,23 @@
+﻿using Swashbuckle.AspNetCore.Annotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab6.Models
+{
+    public class Student
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [SwaggerSchema(ReadOnly = true)]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        public string Program { get; set; }
+    }
+}
